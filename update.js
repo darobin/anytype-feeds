57f2743ce50bd1dc48af7a23a62393d5dbe283b2
Robin Berjon <robin.berjon@nytimes.com>
2025-07-30 15:01:12 +0200

    works badly but it works but badly

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { resolve } from "node:path";
+import { resolve, dirname, join } from "node:path";
 import { readFile } from "node:fs/promises";
 import { program } from 'commander';
 import { FeedManager } from "./lib/feeds.js";
@@ -18,8 +18,10 @@ program
 program
   .argument('<config>', 'path to configuration')
   .action(async (config) => {
-    const data = JSON.parse(await readFile(resolve(config)));
-    const fm = new FeedManager(data);
+    const configPath = resolve(config);
+    const cachePath = join(dirname(configPath), 'cache.json');
+    const data = JSON.parse(await readFile(configPath));
+    const fm = new FeedManager({ ...data, cachePath });
     await fm.update(data.sources);
   })
 ;
